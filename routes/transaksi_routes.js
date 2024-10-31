281d75f97f8d384e8573e98f63e6a74dc79e2670
bintang-bintang <bintangajiwibowo@gmail.com>
2024-10-31 14:25:20 +0700

    build: Fullstack 100%

--- routes/transaksi_routes.js
+++ routes/transaksi_routes.js
@@ -3,15 +3,14 @@ const router = express.Router();
 router.use(express.json());
 
 const transaksiController = require(`../controllers/traksaksi_controller`);
-const { authorize } = require('../controllers/auth_contoller');
+const { authorize } = require("../controllers/auth_contoller");
 const { IsAdmin } = require("../middlewares/role_validation");
 
-
-
-router.post("/",  transaksiController.addTransaksi);
-router.put("/bayar/:id",  transaksiController.handleBayarTransaksi);
-router.get("/",  transaksiController.getTransaksis);
-router.get("/:id",  transaksiController.getTransaksi);
-router.delete("/del",  transaksiController.deleteAllTransaksi);
+router.get("/pdf/:id", transaksiController.PDFdetailTransaksi);
+router.post("/", transaksiController.addTransaksi);
+router.put("/bayar/:id", transaksiController.handleBayarTransaksi);
+router.get("/", transaksiController.getTransaksis);
+router.get("/:id", transaksiController.getTransaksi);
+router.delete("/del", transaksiController.deleteAllTransaksi);
 
 module.exports = router;
